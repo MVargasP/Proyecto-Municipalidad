e3152669e170a83b7cd0b553b1fb20379781e51a
Miguel <migvargasp@gmail.com>
2020-09-12 16:43:50 -0500

    postgres

--- proyMuni/settings.py
+++ proyMuni/settings.py
@@ -81,12 +81,15 @@ WSGI_APPLICATION = 'proyMuni.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE" : "django.db.backends.postgresql_psycopg2",
+        "NAME": 'municipalidaddb',
+        "USER": 'master',
+        "PASSWORD": 'munmaster',
+        "PORT": '5432',
+        "HOST": 'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
